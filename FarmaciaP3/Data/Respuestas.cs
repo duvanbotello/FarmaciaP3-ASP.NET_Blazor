b252f6c3bd0c66e7ea5663acff2cba4ff79de429
DuvanBotello <this.duvan@gmail.com>
2020-06-14 11:59:09 -0500

    Agregar archivos de proyecto.

---
+++ FarmaciaP3/Data/Respuestas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmaciaP3.Data
+{
+    public class Respuestas
+    {
+        public int Exito { get; set; }
+        public string Mensaje { get; set; }
+        public List<Cliente> Data { get; set; }
+    }
+}
---
+++ FarmaciaP3/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using FarmaciaP3.Data;
+using System.Net.Http;
+
+namespace FarmaciaP3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            var HttClientGandler = new HttpClientHandler();
+            HttClientGandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            services.AddSingleton(new HttpClient(HttClientGandler));
+            services.AddSingleton(new HttpClient()
+            {
+                BaseAddress = new System.Uri("https://localhost:44387")
+            }); ;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
